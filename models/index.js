3aa1a4469266f34b2dc8a7d032150a3afdb922a5
Alejandro Ocampo <alejocampo@gmail.com>
2021-06-14 11:02:10 -0500

    commit de prueba

--- models/index.js
+++ models/index.js
@@ -1,12 +1,18 @@
-const mongoose = require("mongoose")
-const bdUrl = "mongodb+srv://alejocampo:15Alicia15@cluster0-oywau.mongodb.net/CRUD?retryWrites=true&w=majority"
-const sim = require("./sim")
+const mongoose = require("mongoose");
+const bdUrl =
+  "mongodb+srv://alejocampo:15Alicia15@cluster0-oywau.mongodb.net/CRUD?retryWrites=true&w=majority";
+const sim = require("./sim");
 
-mongoose.connect(bdUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    err ? console.log(err) : console.log("Conexión exitosa");
-})
-
+mongoose.connect(
+  bdUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    err
+      ? console.log(err)
+      : console.log("Conexión exitosa - conexion exitosa de prueba");
+  }
+);
 
 module.exports = {
-    sim
-}
+  sim,
+};
